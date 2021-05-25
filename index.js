1da62e9c3ff5b22518c3fced83f3ae64062ccae3
Riswan Agyeman <agyeman.riswan@gmail.com>
2021-05-25 15:33:33 -0500

    Created a post route for submitting a form

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/products', async (req, res) => {
 
 })
 
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
+
+app.post('/products', (req, res) => {
+    res.render('products/index', { products })
+})
+
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id)
