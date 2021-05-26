922101758f838bcb0a3518094326cba515a3bfd6
Riswan Agyeman <agyeman.riswan@gmail.com>
2021-05-25 20:05:45 -0500

    Installed the method-override package

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
+app.set(express.urlencoded({ extended: true }))
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
@@ -28,16 +29,21 @@ app.get('/products/new', (req, res) => {
     res.render('products/new')
 })
 
-app.post('/products', (req, res) => {
-    res.render('products/index', { products })
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    newProduct.save();
+    console.log(newProduct)
+    res.redirect(`/products/${newProduct._id}`)
+    //res.send("Making new product")
 })
 
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id)
-    console.log(product)
     res.render('products/show', { product })
 })
+
+
 app.listen(3000, () => {
     console.log("Connected on PORT 3000")
 })
